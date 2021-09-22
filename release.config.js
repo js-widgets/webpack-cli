29adeca31400fa1fc8ca40174fee5df296d6f613
Mateu Aguiló Bosch (e0ipso) <mateu@mateuaguilo.com>
2021-09-22 16:31:47 +0200

    docs: remove changelog

--- release.config.js
+++ release.config.js
@@ -3,17 +3,9 @@ module.exports = {
   plugins: [
     ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits' }],
     '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/changelog',
-      { changelogFile: 'website/src/pages/changelog.md' },
-    ],
+    '@semantic-release/changelog',
     ['@semantic-release/npm', { tarballDir: 'release' }],
     ['@semantic-release/github', { assets: 'release/*.tgz' }],
-    [
-      '@semantic-release/git',
-      {
-        assets: ['website/src/pages/changelog.md'],
-      },
-    ],
+    '@semantic-release/git',
   ],
 };
