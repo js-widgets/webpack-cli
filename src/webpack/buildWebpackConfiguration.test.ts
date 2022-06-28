b82af7b51928f2a9c2cbc150016de7c6d6ab2ca9
Mateu Aguiló Bosch (e0ipso) <mateu@mateuaguilo.com>
2022-06-28 11:09:24 +0200

    test: update snapshots

--- src/webpack/buildWebpackConfiguration.test.ts
+++ src/webpack/buildWebpackConfiguration.test.ts
@@ -1,11 +1,18 @@
 import buildWebpackConfiguration from './buildWebpackConfiguration';
 import loadWidgetDefinitionFile from './widgetDefinition/loadWidgetDefinitionFile';
 import { join as pathJoin } from 'path';
+import configuration from '../defaultConfig/webpack.config';
+import { Configuration } from 'webpack';
 
 describe('buildWebpackConfiguration', () => {
   it('builds the correct configuration with empty data', async () => {
     expect.assertions(1);
-    const actual = await buildWebpackConfiguration([], {}, '', '');
+    const actual = await buildWebpackConfiguration(
+      [],
+      configuration as Configuration,
+      '',
+      '',
+    );
     expect(actual).toMatchSnapshot();
   });
   it('builds the correct configuration', async () => {
@@ -17,7 +24,7 @@ describe('buildWebpackConfiguration', () => {
     ];
     const actual = await buildWebpackConfiguration(
       definitions,
-      {},
+      configuration as Configuration,
       pathJoin(
         __dirname,
         '../__testData__/validProject/.widgetRegistry/main.js',
